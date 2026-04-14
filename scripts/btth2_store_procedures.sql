addf3c2ef48df6e3efdd84be849ab505f2c8df36
Trung Nguyen <nguyenchitrung1310@gmail.com>
2026-04-14 16:43:31 +0700

    [PART 2A_1] Add stored procedure to insert GIAOVIEN with HOCHAM validation

---
+++ scripts/btth2_store_procedures.sql
@@ -0,0 +1,45 @@
+USE BTTH2_DB;
+
+-- PHẦN 2:
+-- A. STORED PROCEDURES VỚI THAM SỐ VÀO
+-- 1. Tham số vào là MSGV, TENGV, SODT, DIACHI, MSHH, NAMHH.
+-- Trước khi insert dữ liệu cần kiểm tra MSHH đã tồn tại trong table HOCHAM
+-- chưa, nếu chưa thì trả về giá trị 0.
+CREATE PROC usp_InsertGiaoVien_2A_1
+(
+    @MSGV int,
+    @TENGV nvarchar(30),
+    @SODT varchar(10),
+    @DIACHI nvarchar(50),
+    @MSHH int,
+    @NAMHH smalldatetime
+)
+AS
+    BEGIN
+        IF NOT EXISTS (SELECT 1 FROM HOCHAM WHERE MSHH = @MSHH)
+        BEGIN
+            print(N'Mã học hàm không tồn tại!')
+            RETURN 0
+        END
+
+        INSERT INTO GIAOVIEN (MSGV, TENGV, DIACHI, SODT, MSHH, NAMHH)
+        VALUES (@MSGV, @TENGV, @DIACHI, @SODT, @MSHH, @NAMHH)
+
+        RETURN 1
+    END
+GO
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
