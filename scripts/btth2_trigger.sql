eee5573c91cd57e56c7e86d6b1412ea929fab43a
Trung Nguyen <nguyenchitrung1310@gmail.com>
2026-04-16 16:59:50 +0700

    [PART 2C_2] Add trigger to update related records when MSGV is changed

--- scripts/btth2_trigger.sql
+++ scripts/btth2_trigger.sql
@@ -18,3 +18,34 @@ AS
     END
 GO
 
+-- 2. Khi đổi MSGV thì cập nhật các bảng liên quan
+CREATE TRIGGER trg_UpdateMSGV
+ON GIAOVIEN
+INSTEAD OF UPDATE
+AS
+    BEGIN
+        IF UPDATE(MSGV)
+        BEGIN
+            DECLARE @OldMSGV INT, @NewMSGV INT;
+
+            SELECT @OldMSGV = MSGV FROM DELETED;
+            SELECT @NewMSGV = MSGV FROM INSERTED;
+
+            -- 1) Tạo bản ghi giáo viên mới trước để FK ở bảng con hợp lệ
+            INSERT INTO GIAOVIEN (MSGV, TENGV, DIACHI, SODT, MSHH, NAMHH)
+            SELECT MSGV, TENGV, DIACHI, SODT, MSHH, NAMHH
+            FROM INSERTED;
+
+            -- 2) Đổi MSGV ở các bảng con từ old -> new
+            UPDATE GV_HV_CN SET MSGV = @NewMSGV WHERE MSGV = @OldMSGV;
+            UPDATE GV_HDDT SET MSGV = @NewMSGV WHERE MSGV = @OldMSGV;
+            UPDATE GV_PBDT SET MSGV = @NewMSGV WHERE MSGV = @OldMSGV;
+            UPDATE GV_UVDT SET MSGV = @NewMSGV WHERE MSGV = @OldMSGV;
+            UPDATE HOIDONG SET MSGV = @NewMSGV WHERE MSGV = @OldMSGV;
+            UPDATE HOIDONG_GV SET MSGV = @NewMSGV WHERE MSGV = @OldMSGV;
+
+            -- 3) Xóa bản ghi giáo viên cũ
+            DELETE FROM GIAOVIEN WHERE MSGV = @OldMSGV;
+        END
+    END
+GO
