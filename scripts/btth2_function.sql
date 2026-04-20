19658d77779a9ae30dbc1bf32d1ea08730bfd02b
NguyenChiTrung1310 <nguyenchitrung1310@gmail.com>
2026-04-20 20:41:55 +0700

    [PART 2D_2] Add fn_KetQuaDeTai function to evaluate DETAI results based on average score

--- scripts/btth2_function.sql
+++ scripts/btth2_function.sql
@@ -9,21 +9,33 @@ CREATE FUNCTION fn_AvgDiemDeTai(
 )
     RETURNS FLOAT
 AS
-BEGIN
-    DECLARE @DiemTrungBinh FLOAT;
+    BEGIN
+        DECLARE @DiemTrungBinh FLOAT;
 
-    SELECT @DiemTrungBinh = CAST(AVG(DIEM) AS DECIMAL(5, 2))
-    FROM (
-        SELECT DIEM FROM GV_HDDT WHERE MSDT = @MSDT
-          UNION ALL
-          SELECT DIEM FROM GV_PBDT WHERE MSDT = @MSDT
-          UNION ALL
-          SELECT DIEM FROM GV_UVDT WHERE MSDT = @MSDT
-    ) AS TongDiemDT
+        SELECT @DiemTrungBinh = CAST(AVG(DIEM) AS DECIMAL(5, 2))
+        FROM (
+            SELECT DIEM FROM GV_HDDT WHERE MSDT = @MSDT
+              UNION ALL
+              SELECT DIEM FROM GV_PBDT WHERE MSDT = @MSDT
+              UNION ALL
+              SELECT DIEM FROM GV_UVDT WHERE MSDT = @MSDT
+        ) AS TongDiemDT
 
-    RETURN ISNULL(@DiemTrungBinh, 0);
-END
+        RETURN ISNULL(@DiemTrungBinh, 0);
+    END
 GO
 
-SELECT sv.MSSV, sv.TENSV, dbo.fn_AvgDiemDeTai(svdt.MSDT) AS DiemTrungBinhDeTai
-FROM SV_DETAI svdt JOIN SINHVIEN sv ON svdt.MSSV = sv.MSSV;
+-- 2. Trả về kết quả của đề tài theo MSDT nhập vào. Kết quả là DAT nếu như
+-- điểm trung bình từ 5 trở lên, và KHONGDAT nếu như điểm trung bình dưới 5.
+CREATE FUNCTION fn_KetQuaDeTai(
+    @MSDT char(6)
+)
+    RETURNS VARCHAR(10)
+AS
+    BEGIN
+        IF dbo.fn_AvgDiemDeTai(@MSDT) >= 5
+            RETURN 'DAT';
+
+        RETURN 'KHONGDAT';
+    END
+GO
